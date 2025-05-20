d5895e8c93d46c714e03c8e484d0bfba47737728
vito <vitonava16@gmail.com>
2025-05-20 09:15:01 -0300

    agregue el primer ejercicio de cadenas al repositorio

---
+++ ej1_cad_tp3.py
@@ -0,0 +1,29 @@
+"""
+Desarrollar una función que determine si una cadena de caracteres es capicúa. 
+Escribir además un programa que permita verificar su funcionamiento solicitándole 
+al usuario valores hasta que el mismo sea vacío.
+"""
+
+def es_capicua(texto): 
+    """
+    obj: determinar si una cadena de caracteres es capicúa
+    entrada(parametro): cad de caracteres
+    salida: True o False
+    """
+    if texto == texto[::-1]:
+        return True
+    else: 
+        return False
+    
+def main():
+    cad = input("ingrese un texto: ")
+
+    while len(cad) > 0:
+        if es_capicua(cad) == True:
+            print(f'la cadena: {cad} es capicua')
+        else: 
+            print(f'la cadena: {cad} no es capicua')        
+        cad = input("ingrese otro texto: ")
+
+if __name__ == "__main__": 
+    main()
