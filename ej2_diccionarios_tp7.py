cab653a5272ff244fdbd8ae2f3d008c1a8c67f89
vito <vitonava16@gmail.com>
2025-05-22 13:39:34 -0300

    cambios en los nombres

--- ej2_diccionarios_tp7.py
+++ ej2_diccionarios_tp7.py
@@ -4,28 +4,38 @@ los almacene en un diccionario y los muestre.
 Al realizar dicha muestra, destacar la persona más joven en edad.
 """
 
-def sacarMenor(dic):
-    for nombre, datos in dic.items():
-        edad = datos["edad"]
-        if edad == min([datos["edad"] for datos in dic.values()]):
-            print(f'\t\t{nombre} - {datos}')
+def sacarMenor(lista):
+    """
+    obj: recorrer la lista de personas y mostrar la persona más joven del diccionario.
+    entrada: lista de personas(diccionarios)
+    salida: imprime la persona más joven
+    """
+    menor = lista[0]
+    for elemento in lista:
+        if elemento.get('edad') < menor.get('edad'):
+            menor = elemento
+    return menor
 
 def main():
-    personas = {}
+    personas = []
     while len(personas) < 3:
         nombre = input("ingrese el nombre de la persona: ")
         edad = int(input("ingrese la edad de la persona: "))
         genero = input("ingrese el genero de la persona: ")
         direccion = input("ingrese la direccion de la persona: ")
         telefono = input("ingrese el telefono de la persona: ")
-        personas[nombre] = {
+        persona = {
+            "nombre": nombre,
             "edad": edad,
             "genero": genero,
             "direccion": direccion,
             "telefono": telefono
         }
+        personas.append(persona)
+    
     print(f'\t\t persona mas joven: ')
-    sacarMenor(personas)
+    masJoven = sacarMenor(personas)
+    print(f'\t\t{masJoven}')
 
 if __name__ == "__main__":
     main()
